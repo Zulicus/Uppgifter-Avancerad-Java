935fd41c17367db6e2623b88a40466279a148b07
Zulicus <70630780+Zulicus@users.noreply.github.com>
2021-09-10 11:43:56 +0200

    slight change

--- Lektionsuppgift3/src/main/Main.java
+++ Lektionsuppgift3/src/main/Main.java
@@ -2,8 +2,6 @@ package main;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
